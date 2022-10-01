aa5c9d3f3f5546cdd824b7754faf250654141266
kangqiao322 <kangqiao803@gmail.com>
2022-10-02 01:57:43 +0800

    all button functionality adn windows

---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MonoBehaviour
+{
+    private bool isActive;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
