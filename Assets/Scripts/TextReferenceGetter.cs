5a47ac9ba4ab3fa5e36766f8a44482e851f6ebaf
kangqiao322 <kangqiao803@gmail.com>
2022-10-01 23:12:11 +0800

    no message

--- Assets/Scripts/TextReferenceGetter.cs
+++ Assets/Scripts/TextReferenceGetter.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//retrieves the text file from folder
 public class TextReferenceGetter : MonoBehaviour
 {
+    private TextAsset textReferenced;
     // Start is called before the first frame update
     void Start()
     {
